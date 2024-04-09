efa6b8ad4f6a2beb47c29a8478d2dcb4468ea5fc
Peet Stander <62140524+PMLS3@users.noreply.github.com>
2024-04-09 13:00:03 +0200

    updats

--- plugins/drag.client.ts
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import VueDragscroll from 'vue-dragscroll'
-import draggable from 'vuedraggable'
-// https://vue-map.netlify.app/
-export default defineNuxtPlugin((nuxtApp) => {
-  // Doing something with nuxtApp
-  nuxtApp.vueApp.use(VueDragscroll)
-  nuxtApp.vueApp.component('draggable', draggable)
-})
--- plugins/maz.client.ts
+++
@@ -1,7 +0,0 @@
-import MazPhoneNumberInput from 'maz-ui/components/MazPhoneNumberInput'
-import { defineNuxtPlugin } from '#app'
-// https://vue-map.netlify.app/
-export default defineNuxtPlugin((nuxtApp) => {
-  // Doing something with nuxtApp
-  nuxtApp.vueApp.component('MazPhoneNumberInput', MazPhoneNumberInput)
-})
