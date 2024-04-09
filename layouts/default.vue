38db1c76302ea69101f62cdb5e66aee7ca1d4146
Peet Stander <62140524+PMLS3@users.noreply.github.com>
2024-04-09 13:29:44 +0200

    updates

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,7 @@
-<script setup lang="ts">
-/**
- * Here we use the useLayoutSwitcher() composable to dynamically insert the selected layout component.
- * In this template, the collpase layout is the default layout.
- * We use the vue :is="" feature to render the layout component dynamically.
- */
-
-const { activeLayoutComponent } = useLayoutSwitcher();
-</script>
-
 <template>
-  <component :is="activeLayoutComponent">
+  <div>
     <suspense>
       <slot />
     </suspense>
-  </component>
+  </div>
 </template>
